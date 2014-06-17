5b2c259f0ba51f50fd3d254c4404169519681c9b
David Kowis <david.kowis@rackspace.com>
2014-06-17 17:25:35 -0500

    Adding a debian specific OS init script
    Differentiating between Ubuntu and Debian in the tests as well

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,14 +3,26 @@
 # This class is called from remotesyslog
 #
 class remotesyslog::config {
-  file { '/etc/init/remote_syslog.conf':
-    source => 'puppet:///modules/remotesyslog/etc/init/remote_syslog.conf'
+  case $::operatingsystem {
+    'Ubuntu': {
+        file { '/etc/init/remote_syslog.conf':
+            source => 'puppet:///modules/remotesyslog/etc/init/remote_syslog.conf'
+        }
+
+        file { '/etc/init.d/remote_syslog':
+            ensure => link,
+            target => '/lib/init/upstart-job',
+        }
+    }
+    'Debian': {
+        file{'/etc/init.d/remote_syslog':
+            ensure => file,
+            source => 'puppet://modules/remotesyslog/etc/init/remote_syslog.init.d',
+        }
+    }
+    default: { fail("Operating System ${::operatingsystem} not supported by remotesyslog!") }
   }
 
-  file { '/etc/init.d/remote_syslog':
-    ensure => link,
-    target => '/lib/init/upstart-job',
-  }
 
   file { '/etc/log_files.yml':
     content => template('remotesyslog/etc/log_files.yml.erb')
